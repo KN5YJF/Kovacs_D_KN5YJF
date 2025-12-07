2e1de7a1a35fc3eafc52726f2ace101795ea1a84
KN5YJF <kn5yjf@neptun.gde.hu>
2025-12-07 21:18:19 +0100

    szintakszis javítás

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');   //Express keretrendszer betöltés
 const app = express();                //Példány létrehozás
-const port = 3000;                    //Port dedikálás
+const port = 3000;                    //Port dedikálás 
 
-app.get('/', (req, res) => {          //elérés kezelés
+app.get('/', (req, res) => {          //elérés kezelés 
+  res.send('Hello DevOps!');          //visszadott szöveg
+});
 
-  res.send('Hello DevOps world! CHG');    //visszadott szöveg
-
-app.listen(port, () => {               //Server start, a meghatározott porton
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, () => {              //port meghatárosás
+  console.log(`App is running on http://localhost:${port}`);
 });
